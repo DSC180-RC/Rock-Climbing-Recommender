bdb5129f5e12f55d14a46a82d22346b0c182c407
Eric Liu <twigoracle@gmail.com>
2021-01-18 12:35:57 -0800

    Made path relative to project root

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -16,6 +16,6 @@ path = "/mysite/"
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.mysite.settings')
 
 application = get_wsgi_application()
