3359ffb43b3d620623c2809c9ed23a3e820c57da
Eric Liu <twigoracle@gmail.com>
2021-01-20 15:48:52 -0800

    Added and integrated function get_route_rating_data

--- src/data/get_raw_data.py
+++ src/data/get_raw_data.py
@@ -15,12 +15,16 @@ from src.functions import make_absolute
 def get_raw_data(data_params):
     """
     This function collates all scraping logic
+
+    :param:     data_params     A dictionary containing all data parameters. The only one used is
+                                the location at which to save raw data
     """
     # store raw data here
     raw_data = []
 
     # for now, just scrape Yosemite routes
     area_url = "https://www.mountainproject.com/area/105833381/yosemite-national-park"
+    # area_url = "https://www.mountainproject.com/area/113603736/the-box" # only 7 climbs!
     all_routes = find_all_routes_in_area(area_url)
 
     # for every route in Yosemite, get the route data
@@ -38,15 +42,79 @@ def get_route_data(route_url):
     TODO: get the users who rated and their ratings for this route
 
     :param:     route_url   The URL at which the route lives
+
+    :return:    
     """
-    # TODO: document function
+    # get the climb description
     text = requests.get(route_url).text
     soup = BeautifulSoup(text, 'html.parser')
+
+    # split up the stuff we want
     data = json.loads("".join(soup.find("script", {"type":"application/ld+json"}).contents))
     description = soup.find('div', {'class': 'fr-view'}).contents
     data['description'] = ''.join([x for x in description if isinstance(x, str)])
     data['route_url'] = route_url
+
+    # if there exists more than zero ratings for this climb, then get those user(s)
+    if(int(data["aggregateRating"]["reviewCount"]) > 0):
+        data["user_ratings"] = get_route_rating_data(route_url)
+    # otherwise just add an empty dict
+    else:
+        data["user_ratings"] = {}
+
     return data
+
+def get_route_rating_data(route_url):
+    """
+    Get all user_ids and the ratings for the input route
+
+    :param:     route_url   The URL at which the route lives. NOTE (!) this is not the URL at which
+                            rating data can be found, it is modified in this function
+    """
+    # first modify the route_url to access the stats
+    route_stats_url = route_url.split("/")
+    route_stats_url.insert(4, "stats")
+    route_stats_url = "/".join(route_stats_url)
+
+    # get the html of the stats page
+    text = requests.get(route_stats_url).text
+    soup = BeautifulSoup(text, 'html.parser')
+
+    # get the first table which should contain ratings
+    ratings_table = soup.find("table", attrs={"class": "table"})
+
+    # make sure that a table exists 
+    if(ratings_table == None):
+        print(route_stats_url)
+        return {}
+
+    # get the rows of the rating table
+    rows = ratings_table.find_all("tr")
+    
+    # store ratings here
+    user_ratings = {}
+
+    # every single row is a users rating
+    for row in rows:
+        # first col contains the user_id
+        cols = row.find_all("td")
+        user_url = cols[0].find("a").get("href")
+        user_id = user_url.split("/")[4]
+
+        # second col contains the user rating (0-4 stars)
+        stars = len(cols[1].find_all("img"))
+
+        # the above interprets avoid (single image of a bomb) the same as one star (single image
+        # of a star)
+        # adjust for that
+        if(stars == 1 and ("bomb" in cols[1].find("img")["src"])):
+            stars = 0
+        
+        # store the rating
+        user_ratings[user_id] = stars
+            
+    # return the ratings
+    return user_ratings
 
 def get_user_history(user_url):
     """
