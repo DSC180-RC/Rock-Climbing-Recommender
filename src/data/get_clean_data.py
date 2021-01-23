2474c1b3ca3373c2ab656d9aa7e5a438b0913449
Eric Liu <twigoracle@gmail.com>
2021-01-23 15:39:10 -0800

    Added new command line argument and created structure

---
+++ src/data/get_clean_data.py
@@ -0,0 +1,15 @@
+# Brian Cheng
+# Eric Liu
+# Brent Min
+
+# get_clean_data.py collates all the logic needed to clean and save data
+
+def get_clean_data(data_params):
+    """
+    This function collates all cleaning logic
+
+    :param:     data_params     A dictionary containing all data parameters. The only ones used are
+                                the location at which to download raw data and the location at which
+                                to save clean data
+    """
+    print(data_params)
