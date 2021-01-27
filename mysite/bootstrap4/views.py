bf21c33c623ace8e4673c0c05624e01fa80cba3f
Eric Liu <twigoracle@gmail.com>
2021-01-26 18:59:01 -0800

    First try:

--- mysite/bootstrap4/views.py
+++ mysite/bootstrap4/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def bootstrap4_index(request):
+    if(request.method == "POST" and "run_script" in request.POST):
+        from ...src.test_function import test_function
+        x = test_function(5)
+        return HttpResponse("test run")
+
     return render(request, 'index.html', {})
