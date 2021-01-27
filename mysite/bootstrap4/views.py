37f5efe5ce575da50f8be8228fc5dcad3942a01c
Eric Liu <twigoracle@gmail.com>
2021-01-26 19:04:04 -0800

    Fixed path issue

--- mysite/bootstrap4/views.py
+++ mysite/bootstrap4/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+import sys
+sys.path.append('../../..')
+
 def bootstrap4_index(request):
     if(request.method == "POST" and "run_script" in request.POST):
-        from ...src.test_function import test_function
+        from src.test_function import test_function
         x = test_function(5)
         return HttpResponse("test run")
 
