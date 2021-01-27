38341c88d0980767ed8fd49542edfbf4b1196ddd
Eric Liu <twigoracle@gmail.com>
2021-01-26 19:08:30 -0800

    Added a p tag to modify

--- mysite/bootstrap4/views.py
+++ mysite/bootstrap4/views.py
@@ -7,6 +7,6 @@ def bootstrap4_index(request):
     if(request.method == "POST" and "run_script" in request.POST):
         from src.test_function import test_function
         x = test_function(5)
-        return HttpResponse("test run")
+        return render(request, 'index.html', {"test": x})
 
-    return render(request, 'index.html', {})
+    return render(request, 'index.html', {"test": "test"})
