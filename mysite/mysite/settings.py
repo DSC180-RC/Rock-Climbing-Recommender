f75be4e70a9bac7e2f28c8d32f074cdad055c893
Eric Liu <twigoracle@gmail.com>
2021-02-15 17:30:40 -0800

    Added mimetype import

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+# allow for the adding of static files
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
