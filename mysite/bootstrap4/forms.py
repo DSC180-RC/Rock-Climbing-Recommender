95b66ce096fee6b01e7d58cc6bbee0093c1c0f3d
Eric Liu <twigoracle@gmail.com>
2021-02-15 20:01:08 -0800

    More error checking

--- mysite/bootstrap4/forms.py
+++ mysite/bootstrap4/forms.py
@@ -12,25 +12,10 @@ class RecInputForm(NoColon):
     rec = forms.MultipleChoiceField(label="Recommenders:", choices=(
         ("top_pop", "Top Popular"),
         ("other", "Other Recommender")))
+    num_recs = forms.IntegerField(label="Number of Recommendations:", initial=10)
     boulder_lower = forms.IntegerField(label="Lowest Boulder Grade: V", min_value=0, max_value=16, 
         initial=0)
     boulder_upper = forms.IntegerField(label="Highest Boulder Grade: V", min_value=0, max_value=16,
         initial=3)
     route_lower = forms.CharField(label="Lowest Route Grade: 5.", max_length=3, initial="8")
     route_upper = forms.CharField(label="Highest Route Grade: 5.", max_length=3, initial="10d")
-
-    def clean(self):
-        """
-        This first runs default cleaning code (as defined by the field type and various parameters)
-        then runs some custom cleaning code in order to verify:
-        1. The boulder_lower is a lower or equal difficulty to the boulder_upper
-        2. Both route_lower and route_upper contain valid difficulty strings
-        3. The route_lower is a lower or equal difficulty to the route_upper 
-        """
-        # do the default form cleaning
-        cleaned_data = super().clean()
-
-        # make sure that boulder_lower is lower or equal difficulty to boulder_upper
-        if(cleaned_data.get("boulder_lower") > cleaned_data.get("boulder_upper")):
-            self.add_error("boulder_lower", forms.ValidationError("Lowest Boulder Grade should be less "
-                "than or equal to Highest Boulder Grade"))
--- mysite/bootstrap4/views.py
+++ mysite/bootstrap4/views.py
@@ -29,12 +29,12 @@ def bootstrap4_index(request):
 
         if(form.is_valid()):
 
-            # create the config dictionary to pass into main
-            inputs = {
-                "user_url": request.POST.get("url"),
-                "location": [request.POST.get("latitude"), request.POST.get("longitude")],
-                "recommender": request.POST.get("rec")
-            }
+            # run the secondary validation code
+            inputs = secondary_validation(request)
+
+            # if there are errors, then a string would be returned
+            if(isinstance(inputs, str)):
+                return render(request, 'index.html', template(form, inputs))
 
             # run the main code
             from run import main
@@ -48,3 +48,72 @@ def bootstrap4_index(request):
 
     form = RecInputForm()
     return render(request, 'index.html', template(form))
+
+def secondary_validation(request):
+    """
+    This function runs some secondary validation code that I could not integrate into django
+    without it messing up the website style
+
+    :param:     request     The POST request
+
+    :return:    dict        A formatted dictionary containing all information needed to make a 
+                            recommendation
+    :return:    str         If there is some error, return a string to display to the user
+    """
+    # store error string here if necessary
+    error_str = ""
+
+    # get the boulder grades
+    bl = request.POST.get("boulder_lower")
+    bu = request.POST.get("boulder_upper")
+
+    # validate the boulder grades
+    if(bl > bu):
+        error_str += "Lowest Boulder Grade should be less than or equal to Highest Boulder Grade.\n"
+
+    # get the route grades
+    rl = route_to_int(request.POST.get("route_lower"))
+    ru = route_to_int(request.POST.get("route_upper"))
+
+    # validate the route grades
+    if(rl is None):
+        error_str += "Lowest Route Grade is an invalid difficulty.\n"
+    if(ru is None):
+        error_str += "Highest Route Grade is an invalid difficulty.\n"
+    if((rl is not None) and (ru is not None)):
+        if(rl > ru):
+            error_str += "Lowest Route Grade should be less than or equal to Highest Route Grade.\n"
+
+    # if some errors were encountered, return the error string
+    if(not error_str):
+        return error_str
+
+    # create the config dictionary to pass into main
+    inputs = {
+        "user_url": request.POST.get("url"),
+        "location": [request.POST.get("latitude"), request.POST.get("longitude")],
+        "recommender": request.POST.get("rec"),
+        "num_recs": request.POST.get("num_recs"),
+        "difficulty_range": {
+            "boulder": [bl, bu],
+            "route": [rl, ru]
+        }
+    }
+    return inputs
+
+def route_to_int(route_str):
+    """
+    This function takes a route string and turns it into an integer
+
+    :param:     route_str   The stuff after the "5.". Can be anything from "1" to "15d"
+
+    :return:    int         An integer representation of the grade
+    """
+    mapping = ["1", "2", "3", "4", "5", "6", "7", "", "8", "9", "10a", "10b", "10c", "10d", "11a", 
+        "11b", "11c", "11d", "12a", "12b", "12c", "12d", "13a", "13b", "13c", "13d", "14a", "14b", 
+        "14c", "14d", "15a", "15b", "15c", "15d"]
+
+    try:
+        return mapping.index(route_str)
+    except ValueError:
+        return None
