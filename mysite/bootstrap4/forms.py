035c7327ce5f6257fe454681c3d53dd1ee1f48ce
Eric Liu <twigoracle@gmail.com>
2021-02-15 19:07:39 -0800

    First attempt at custom validation

--- mysite/bootstrap4/forms.py
+++ mysite/bootstrap4/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from django.core.exceptions import ValidationError
+
 class NoColon(forms.Form):
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('label_suffix', '')
@@ -18,3 +20,21 @@ class RecInputForm(NoColon):
         initial=3)
     route_lower = forms.CharField(label="Lowest Route Grade: 5.", max_length=3, initial="8")
     route_upper = forms.CharField(label="Highest Route Grade: 5.", max_length=3, initial="10d")
+
+    def clean(self):
+        """
+        This first runs default cleaning code (as defined by the field type and various parameters)
+        then runs some custom cleaning code in order to verify:
+        1. The boulder_lower is a lower or equal difficulty to the boulder_upper
+        2. Both route_lower and route_upper contain valid difficulty strings
+        3. The route_lower is a lower or equal difficulty to the route_upper 
+        """
+        # do the default form cleaning
+        super().clean()
+
+        # make sure that boulder_lower is lower or equal difficulty to boulder_upper
+        bl = self.cleaned_data.get("boulder_lower")
+        bu = self.cleaned_data.get("boulder_upper")
+        if(bl > bu):
+            raise ValidationError("Lower Boulder Grade should be less than or equal to Highest "
+                "Boulder Grade")
--- mysite/bootstrap4/views.py
+++ mysite/bootstrap4/views.py
@@ -24,25 +24,12 @@ def template(form=None, rec="", latitude=33.8734, longitude=-115.9010):
 def bootstrap4_index(request):
     # enter the if if the button is pressed on the website
     if(request.method == "POST"):
-        # # import the main function and run it with the selected options
-        # from run import main
-        # config = {"mongodb": True, "top_pop": True}
-        # # result = main(config)
-
-        # test = {"user_url": request.POST.get("user_url"),
-        #     "lat": request.POST.get("lat"),
-        #     "long": request.POST.get("long"),
-        #     "num_rec": request.POST.get("num-rec"),
-        #     "recommenders": [request.POST.get("top-pop"), request.POST.get("other-rec")]}
-
-        # # return the template but with the returned contents of main
-        # return render(request, 'index.html', {"test": test})
 
         form = RecInputForm(request.POST)
 
         if(form.is_valid()):
 
-            # create the config dictionary to pass into amin
+            # create the config dictionary to pass into main
             inputs = {
                 "user_url": request.POST.get("url"),
                 "location": [request.POST.get("latitude"), request.POST.get("longitude")],
