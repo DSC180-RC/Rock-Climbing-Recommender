db449333a3f83e54181909561ef34cbee1e31ec6
Eric Liu <twigoracle@gmail.com>
2021-02-15 18:36:04 -0800

    Removed trailing colon from label

--- mysite/bootstrap4/forms.py
+++ mysite/bootstrap4/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
-class RecInputForm(forms.Form):
-    url = forms.URLField(label="Mountain Project URL", max_length=100)
-    latitude = forms.DecimalField(label="Latitude", initial=33.8734)
-    longitude = forms.DecimalField(label="Longitude", initial=-115.9010)
-    rec = forms.MultipleChoiceField(label="Recommenders", choices=(
+class NoColon(forms.Form):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super(NoColon, self).__init__(*args, **kwargs)
+
+class RecInputForm(NoColon):
+    url = forms.URLField(label="Mountain Project UR:L", max_length=100)
+    latitude = forms.DecimalField(label="Latitude:", initial=33.8734)
+    longitude = forms.DecimalField(label="Longitude:", initial=-115.9010)
+    rec = forms.MultipleChoiceField(label="Recommenders:", choices=(
         ("top_pop", "Top Popular"),
         ("other", "Other Recommender")))
     boulder_lower = forms.IntegerField(label="Lowest Boulder Grade: V")
