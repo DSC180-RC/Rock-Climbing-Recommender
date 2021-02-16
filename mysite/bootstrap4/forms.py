0589969347efe1f0422b2087b1266fe8b2cde3cd
Eric Liu <twigoracle@gmail.com>
2021-02-15 18:33:03 -0800

    Added prefixes to difficulty form elements

--- mysite/bootstrap4/forms.py
+++ mysite/bootstrap4/forms.py
@@ -7,7 +7,7 @@ class RecInputForm(forms.Form):
     rec = forms.MultipleChoiceField(label="Recommenders", choices=(
         ("top_pop", "Top Popular"),
         ("other", "Other Recommender")))
-    boulder_lower = forms.IntegerField(label="Lowest Boulder Grade")
-    boulder_upper = forms.IntegerField(label="Highest Boulder Grade")
-    route_lower = forms.IntegerField(label="Lowest Route Grade")
-    route_upper = forms.IntegerField(label="Highest Route Grade")
+    boulder_lower = forms.IntegerField(label="Lowest Boulder Grade: V")
+    boulder_upper = forms.IntegerField(label="Highest Boulder Grade: V")
+    route_lower = forms.IntegerField(label="Lowest Route Grade: 5.")
+    route_upper = forms.IntegerField(label="Highest Route Grade: 5.")
